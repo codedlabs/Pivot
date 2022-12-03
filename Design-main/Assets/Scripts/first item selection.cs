42f9c387be5858b6447ea534c1629c3fa8072bac
RazanAlmutairi <115035851+RazanAlmutairi@users.noreply.github.com>
2022-12-03 11:22:23 +0300

    razanupdatre

---
+++ Design-main/Assets/Scripts/first item selection.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class firstitemselection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public Text textBox;
+    public Dropdown dropdown;
+    public int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        dropdown = transform.GetComponent<Dropdown>();
+        dropdown.options.Clear();
+        List<string> items = new List<string>();
+        items.Add("Options");
+        items.Add("IKEA");
+        items.Add("Abyat");
+        items.Add("Midas");
+        
+
+        foreach (var item in items)
+        {
+            dropdown.options.Add(new Dropdown.OptionData() { text = item });
+
+        }
+        dropdown.onValueChanged.AddListener(delegate { dropdownitemselected(dropdown); });
+    }
+    void dropdownitemselected(Dropdown dropdown)
+    {
+        index = dropdown.value;
+        textBox.text = dropdown.options[index].text;
+    }
+}
+
+   
+}
